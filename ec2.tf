61e8b4f7662e1be3e59db22a32ff7ef99ac5047c
vaishnavi mule <vaishnavimule128@gmail.com>
2026-05-30 23:59:30 +0530

    new file added

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "eu-north-1"
+  access_key = "esdds"
+  secret_key = "gjakm"
+}
+resource "aws_instance" "my-instance" {
+  ami ="ami-00263659a97a6c29c"
+  instance_type = "t3.micro"
+  key_name = "key-rsa"
+  vpc_security_group_ids = [security-group]
+    tags = {
+    Name = "my-instance"
+    name = "instance"
+    environment = "DevOps"
+    }
+}
